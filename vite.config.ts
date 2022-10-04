60015f21d10d11d2183943106f863cd3a7407a2c
unknown <urakmkt@yahoo.com.br>
2022-10-04 10:45:31 -0300

    corrige base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./bitcoin-spa",
+  base: "/bitcoin-spa/",
   plugins: [vue()],
   resolve: {
     alias: {
