cfac2f461868e319c5f60e11993ec96cead1f4c7
unknown <urakmkt@yahoo.com.br>
2022-10-04 10:55:57 -0300

    ajusta deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -21,7 +21,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+git push -f git@github.com:UrakFerraz/bitcoin-spa.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:UrakFerraz/bitcoin-spa.git master:gh-pages
